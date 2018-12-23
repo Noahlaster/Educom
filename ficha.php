85279e93522767747198d942a74c365586c0aa3f
Jhonnata <jhonnata38@hotmail.com>
2018-12-23 21:28:11 -0200

    Rodapé das páginas consertado

--- ficha.php
+++ ficha.php
@@ -4,14 +4,34 @@
         <meta charset="UTF-8">
         <title>Fichas</title>
         <link rel="icon" href="img/icon.png">
+        <link rel="stylesheet" type="text/css" href="style/ficha.css">
     </head>
     <body>
+    <header>
+        <nav>
+            <img src="img/logo1.png">
+            <a class="alink" href="home.php">Início</a>
+            <div class="divnav">
+                <form class="fornav" method="post" action="action_php/action_home.php">
+                    <input class="input" type="submit" value="Sair" name="sair">
+                </form>
+            </div>
+        </nav>
+    </header>
+    <section>
+        <br>
         <h1>Fichas</h1>
-            <p>Clique para fazer o download.</p>
+        <hr>
+        <div>
+            <br>
             <ul><a href="docs/amnese.pdf" download>Amnese (Adulto/Idoso)</a></ul>
             <ul><a href="docs/triagem.pdf" download>Ficha de Triagem</a></ul>
             <ul><a href="docs/Relatório%20de%20Avaliação%20Psicológica.docx" download>Relatório de Avaliação Psicológica</a></ul>
-
+        </div>
+    </section>
+    <footer>
+        <p>Create by Jhonnata Novaes</p>
+    </footer>
     </body>
 </html>
 <?php
